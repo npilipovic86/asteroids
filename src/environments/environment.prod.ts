d064ddccccec7e8bca265c495a769f1f688dad90
npilipovic86 <npilipovic86@gmail.com>
2019-03-18 13:59:13 +0100

    init2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   path: 'https://api.nasa.gov/neo/rest/v1/feed',
-  apiKey: 'x0HeIJzRCLm3lj0zrfXt2LltusKVCO7aoHmRkVq2's
+  apiKey: 'x0HeIJzRCLm3lj0zrfXt2LltusKVCO7aoHmRkVq2',
 };
