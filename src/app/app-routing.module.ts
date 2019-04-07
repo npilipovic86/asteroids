2153c6a034c748404ac81f76551c9ff5208ae927
npilipovic86 <npilipovic86@gmail.com>
2019-04-07 23:22:28 +0200

    init4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import { MainComponent } from './component/main/main.component';
 import { ViewChartComponent } from './component/view-chart/view-chart.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  // { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: 'main', component: MainComponent },
   { path: 'charts', component: ViewChartComponent },
-  { path: '**', redirectTo: '/', pathMatch: 'full' }
+  { path: '**', redirectTo: 'main', pathMatch: 'full' }
 ];
 
 @NgModule({
   // imports: [RouterModule.forRoot(routes, { enableTracing: false } )],
-  imports: [RouterModule.forRoot(routes)], // , { useHash: true }
+  imports: [RouterModule.forRoot(routes , { useHash: true })], // , { useHash: true }
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
