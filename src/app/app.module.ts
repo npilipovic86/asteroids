c40d3494beb9a691d7b398e93bd2a0704ca5c1c2
npilipovic86 <npilipovic86@gmail.com>
2019-04-07 22:39:27 +0200

    init2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { MainComponent } from './component/main/main.component';
 import { ViewChartComponent } from './component/view-chart/view-chart.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AsteroidService } from './service/asteroid.service';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, MainComponent, ViewChartComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
-  providers: [AsteroidService],
+  providers: [AsteroidService, { provide: LocationStrategy, useClass: PathLocationStrategy } ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
