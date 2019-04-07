fc000d1d5f90154090ca3b179b4f537a715a1172
npilipovic86 <npilipovic86@gmail.com>
2019-04-08 00:29:07 +0200

    init5

--- README.md
+++
@@ -1,7 +0,0 @@
-# asteroids
-
-<br/>[DEMO](https://asteroids17.herokuapp.com)
-
-How to run:
- 1) npm install
- 2) ng serve 
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  path: 'https://api.nasa.gov/neo/rest/v1/feed',
-  apiKey: 'x0HeIJzRCLm3lj0zrfXt2LltusKVCO7aoHmRkVq2',
-};
